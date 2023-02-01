76a248f1f67cdaf1e9f8decd826fe99117bb0605
anhtan7d123 <tanltn123456@gmail.com>
2023-02-01 21:15:14 +0700

    initial commit

---
+++ src/test/java/com/example/learsecup1/Learsecup1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learsecup1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Learsecup1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
